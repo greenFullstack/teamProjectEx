e15abd2a5055d1cd2dd7129b1f950d79d4b877ed
YOONSEOGREEN <yoonseo35@gmail.com>
2024-08-23 10:26:40 +0900

    Changes

---
+++ src/main/java/com/study/teamprojectex/TeamProjectExApplication.java
@@ -0,0 +1,13 @@
+package com.study.teamprojectex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProjectExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamProjectExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/teamprojectex/TeamProjectExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.teamprojectex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProjectExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
